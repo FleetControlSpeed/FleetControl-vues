e08c21c3b735ffd21b3e992d7b79ce73219edf30
Ademar Ramalho <ademar.neto@descomplica.com.br>
2023-06-13 19:51:52 -0300

    Criando models

---
+++ fleetcontrol-vues/src/models/enuns/cor.ts
@@ -0,0 +1,12 @@
+export enum Cor{
+    AMARELO = "AMARELO",
+    BEGE = "BEGE",
+    BRANCA = "BRANCA",
+    CINZA = "CINZA", 
+    MARROM = "MARROM",
+    AZUL = "AZUL",
+    PRATA = "PRATA",
+    PRETA = "PRETA",
+    VERDE = "VERDE",
+    VERMELHO = "VERMELHO"
+}
