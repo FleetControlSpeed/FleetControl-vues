96c5c1da01e17837d25cfe19b0b1d72657364b87
Ademar Ramalho <ademar.neto@descomplica.com.br>
2023-06-13 19:59:49 -0300

    Commit models, marca e tipo

---
+++ fleetcontrol-vues/src/models/enuns/marca.ts
@@ -0,0 +1,12 @@
+export enum Marca{
+    FIAT = "FIAT",
+    CHEVROLET = "CHEVROLET",
+    VOLKSWAGEN = "VOLKSWAGEN",
+    TOYOTA = "TOYOTA",
+    HYUNDAI = "HYUNDAI",
+    RENAULT = "RENAULT",
+    HONDA = "HONDA",
+    VOLVO = "VOLVO",
+    SCANIA = "SCANIS",
+    MERCEDES = "MERCEDES"
+} 
